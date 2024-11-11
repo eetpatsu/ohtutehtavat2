12acf40717615b8f319d00e2e1e6bf516ebd350e
eetpatsu <eetpatsu@student.jyu.fi>
2024-11-11 16:49:53 +0200

    Tehtävä 8. Riippuvuuksien hyödyntäminen

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomllib
 
 
 class ProjectReader:
@@ -9,7 +10,17 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        # print("Content", content)
+
+        sisalto = tomllib.loads(content)
+        # print("Deserialisoitu", sisalto)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(
+            sisalto['tool']['poetry']['name'],
+            sisalto['tool']['poetry']['description'],
+            sisalto['tool']['poetry']['license'],
+            sisalto['tool']['poetry']['authors'],
+            list(sisalto['tool']['poetry']['dependencies'].keys()),
+            list(sisalto['tool']['poetry']['group']['dev']['dependencies'].keys())
+        )
